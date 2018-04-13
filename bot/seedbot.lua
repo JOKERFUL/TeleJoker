c0de33fb0f746fe36a1e1fbc882671be5eaf1a0f
JOKERFUL <38350201+JOKERFUL@users.noreply.github.com>
2018-04-13 05:02:07 -0600

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,32 +224,20 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "Expire"			
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {498475110,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[TeleJoker v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/JOKERFUL/TeleJoker
 
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+Persian: @TeleJokerCh
 ]],
     help_text_realm = [[
 Realm Commands:
